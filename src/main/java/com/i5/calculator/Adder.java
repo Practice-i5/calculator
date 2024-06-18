bc90bb337a2e45c41729a7d0d837201c2bba8452
JungHyeonmin <2ee42a45@gmail.com>
2024-06-18 16:40:23 +0900

    계산기 메인
    숫자 두개를 입력 받은 후 스위치~케이스 문을 이용해서 계산 후 결과 출력

--- src/main/java/com/i5/calculator/Adder.java
+++ src/main/java/com/i5/calculator/Adder.java
@@ -1,4 +1,13 @@
 package com.i5.calculator;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Adder {
+    public static void main(String[] args) {
+        System.out.println(add(2, 3));
+    }
+
+    static public int add(int first, int second) {
+        return first + second;
+    }
 }
--- src/main/java/com/i5/calculator/Divider.java
+++ src/main/java/com/i5/calculator/Divider.java
@@ -1,4 +1,5 @@
 package com.i5.calculator;
 
 public class Divider {
+     // IllegalArgumentException 에러
 }
