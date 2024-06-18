74f855285822bcccb840528f810ec80ee74fd646
JungHyeonmin <2ee42a45@gmail.com>
2024-06-18 18:17:05 +0900

    mod 연산
    숫자 두개를 입력 받은 후 나머지를 반환한다.

--- src/main/java/com/i5/calculator/Moder.java
+++ src/main/java/com/i5/calculator/Moder.java
@@ -1,19 +1,19 @@
 package com.i5.calculator;
 
 public class Moder {
+    public static void main(String[] args) {
+        System.out.println(mod(2, 0));
+    }
+
     static public int mod(int firstNum, int secondNum) {
         int result = 0;
-//        try {
-//            result = firstNum % secondNum;
-//        } catch (IllegalArgumentException e) {
-//            System.out.println("second에 0을 입력하지 마세요");
-//        }
 
-        if (secondNum == 0) {
-            System.out.println("secondNum가 0입니다.");
+        try {
+            result = firstNum % secondNum;
+        } catch (ArithmeticException e) {
+            System.out.println("secondNum에 0을 입력했습니다.");
             return -1;
         }
-
         return result;
     }
 }
