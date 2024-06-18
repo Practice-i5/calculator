1fcf9e295508e54aae0b52635235dfc023522cb0
JungHyeonmin <2ee42a45@gmail.com>
2024-06-18 17:38:18 +0900

    mod 연산
    숫자 두개를 입력 받은 후 나머지를 반환한다.

---
+++ src/test/java/com/i5/calculator/ModerTests.java
@@ -0,0 +1,23 @@
+package com.i5.calculator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ModerTests {
+    @Test
+    @DisplayName("2를 2으로 나눈 나머지를 계산. 0이 나온다.")
+    public void testMod() {
+        // given
+        int first = 2;
+        int second = 2;
+
+        // when
+        int result = new Moder().mod(first, second);
+
+        // then
+        Assertions.assertEquals(0, result);
+    }
+}
